1c63df4fae21548b1d82afe88af884229d91df5d
Nguyen Tuan Khanh <nguyentuankhanhcqt@gmail.com>
2023-02-16 13:33:28 +0700

    update

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-from authentication import Authentication
+from src.authentication import Authentication
 
 # Registering blueprints
 app.register_blueprint(Authentication)
--- src/authentication/__init__.py
+++ src/authentication/__init__.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
 
-Authentication = Blueprint('auth', __name__, url_prefix="/auth")
+Authentication = Blueprint('auth', __name__, url_prefix="/api/auth")
 
 # GET api/auth/register
-Authentication.route('/register', method= ["GET"])
+@Authentication.route('/register')
 def register():
     return jsonify({"data": "register"})
