ff53c254d2c87d4f98edc09803463147df4c9cb9
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2023-03-31 15:50:34 +0700

    fix buf WCC-82

--- src/user/routes.py
+++ src/user/routes.py
@@ -11,6 +11,7 @@ from datetime import datetime
 
 User = Blueprint("user", __name__)
 
+
 # GET api/user
 @User.route("", methods=["GET"])
 @jwt_required()
@@ -88,6 +89,7 @@ def GetUserList():
             "ResponseData": None,
         }
 
+
 @User.route("/add", methods=["POST"])
 @admin_required()
 def AddNewUser():
@@ -162,6 +164,7 @@ def AddNewUser():
             "Description": f"Có lỗi ở máy chủ. \nKhông thể thêm user mới.",
             "ResponseData": None,
         }
+
 
 @User.route("/delete", methods=["DELETE"])
 @admin_required()
