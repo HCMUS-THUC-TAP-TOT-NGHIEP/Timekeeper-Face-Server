6b765b62a20ea8081994a10981080a04d7f75c25
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2023-06-30 23:01:00 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from src import create_app
 import threading
-app = create_app()
 
 if __name__ == '__main__':
     # app.run(threaded=True)
-    app.run()
+    try:
+        app = create_app()
+        app.run()
     # threading.Thread(target=lambda: app.run(
     #     debug=True, use_reloader=False)).start()
+    except Exception as ex:
+        print(f"Failed to run server.Excaption{ex}")
